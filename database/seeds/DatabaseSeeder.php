04d830d8e2ec56aed1717b1db83b74ebbb0a01eb
Matias Bacchiddu <matidanibacchiddu@gmail.com>
2020-10-13 18:24:03 -0300

    Seed de Materias

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(UserSeed::class);
         $this->call(NovedadSeed::class);
+        $this->call(MateriaSeed::class);
     }
 }
